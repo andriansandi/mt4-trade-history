c06a4763663b6314f7e83070d491fe7a3d820c62
Sandi Andrian <sandi.andrian@wundermanthompson.com>
2023-08-19 10:02:45 +0700

    Supabase x MetaApi success

--- supabase/functions/mt4-historical-trade/index.ts
+++ supabase/functions/mt4-historical-trade/index.ts
@@ -1,14 +1,46 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import { Client } from "https://deno.land/x/mysql/mod.ts";
-import * as mod from "https://deno.land/std@0.192.0/uuid/mod.ts";
 import axiod from "https://deno.land/x/axiod/mod.ts";
-import moment from "https://deno.land/x/momentjs@2.29.1-deno/mod.ts";
 
+// Configuration
+const ApiKey = Deno.env.get('METAAPI_APIKEY');
+const AccountId = Deno.env.get('METAAPI_ACCOUNT');
 
-async function getDataFromMetaApi() {
-  
+async function fetchHistoricalTrades() {
+  const apiUrl = `https://metastats-api-v1.london.agiliumtrade.ai/users/current/accounts/${AccountId}/historical-trades/2023-08-17%2000%3A00%3A00/2023-08-17%2020%3A00%3A00?updateHistory=true`;
+
+  console.log(`ApiKey: ${ApiKey}`);
+  console.log(`AccountId: ${AccountId}`);
+
+  try {
+    const response = await axiod.get(apiUrl, {
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": ApiKey,
+      },
+    });
+
+    console.log('Response data:', response.data); // Log the content of response.data
+
+    // Assuming response.data is already an object
+    const trades = response.data;
+    return trades;
+  } catch (error) {
+    console.error('An error occurred:', error.message);
+    throw error; // Rethrow the error to be caught later
+  }
 }
 
 serve(async (server) => {
-  return new Response("Maho");
+  try {
+    const tradeHistory = await fetchHistoricalTrades();
+    console.log(JSON.stringify(tradeHistory));
+    return new Response("Success", { status: 200 }); // Send the fetched data as the response
+  } catch (error) {
+    return new Response("An error occurred.", { status: 500 }); // Handle the error response
+  }
 });
+
+
+
+
+
